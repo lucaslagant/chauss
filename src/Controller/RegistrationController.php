f2101d0a0abf29328cfb972c295e36658377feaa
lucaslagant <lucaslagant@gmail.com>
2023-04-28 12:02:22 +0200

    génèration JWT

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Users;
 use App\Form\RegistrationFormType;
 use App\Security\UsersAuthenticator;
+use App\Service\JWTService;
 use App\Service\SendMailService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -18,7 +19,7 @@ use Symfony\Contracts\Translation\TranslatorInterface;
 class RegistrationController extends AbstractController
 {
     #[Route('/inscription', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, UsersAuthenticator $authenticator, EntityManagerInterface $entityManager, SendMailService $mail): Response
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, UsersAuthenticator $authenticator, EntityManagerInterface $entityManager, SendMailService $mail, JWTService $jwt): Response
     {
         $user = new Users();
         $form = $this->createForm(RegistrationFormType::class, $user);
@@ -36,6 +37,24 @@ class RegistrationController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
             // do anything else you need here, like send an email
+
+            //On génère le JWT de l'utilisateur
+
+            //On crée le Header
+            $header = [
+                'typ' => 'JWT',
+                'alg' => 'HS256S'
+            ];
+
+            //On crée le payload
+            $payload = [
+                'user_id' => $user->getId()
+            ];
+
+            //On génère le token
+            $token = $jwt->generate($header, $payload, $this->getParameter('app.jwtsecret'));
+
+            dd($token);
 
             //On envoie un mail
             $mail->send(
---
+++ src/Service/JWTService.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Service;
+
+use DateTimeImmutable;
+
+class JWTService
+{
+    //On génère le token
+
+    public function generate(array $header, array $payload, string $secret, int $validity = 10800): string
+    {
+        if ($validity <= 0) {
+            return "";
+        }
+
+        $now = new DateTimeImmutable();
+        $exp = $now->getTimestamp() + $validity;
+
+        $payload['iat'] = $now->getTimestamp();
+        $payload['exp'] = $exp;
+
+        //  On encorde en base64
+        $base64Header = base64_encode(json_encode($header));
+        $base64Payload = base64_encode(json_encode($payload));
+
+        //On "nettoie" les valeurs encodées (retrait des + / et = )
+        $base64Header = str_replace(['+', '/', '='], ['-', '_', ''] , $base64Header);
+        $base64Payload = str_replace(['+', '/', '='], ['-', '_', ''] , $base64Payload);
+        
+        //On génère la signature
+        $secret = base64_encode($secret);
+
+        $signature = hash_hmac('sha256', $base64Header . '.' . $base64Payload, $secret, true);
+
+        $base64Signature = base64_encode($signature);
+
+        $base64Signature = str_replace(['+', '/', '='], ['-', '_', ''] , $base64Signature);
+
+        //On créé le token
+        $jwt = $base64Header . '.' . $base64Payload . '.' . $base64Signature;
+
+        return $jwt;
+    }
+}
